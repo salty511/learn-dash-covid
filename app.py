221533f5e6675e761c9fe3391f9eac06bfc49137
Isaac <isaac.w.salt@gmail.com>
2025-07-09 21:43:40 +0100

    radio items for y column selection

--- app.py
+++ app.py
@@ -12,17 +12,26 @@ app = Dash()
 app.layout = [
     html.H1(children="Covid Dashboard", style={"textAlign": "center"}),
     dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
+    dcc.RadioItems(
+        options=["total_cases", "new_cases", "new_cases_smoothed_per_million"],
+        value="total_cases",
+        id="controls-and-radio-item",
+    ),
     dcc.Graph(id="graph-content"),
 ]
 
 
-@callback(Output("graph-content", "figure"), Input("dropdown-selection", "value"))
-def update_graph(value):
-    dff = df[df.country == value]
+@callback(
+    Output("graph-content", "figure"),
+    Input("dropdown-selection", "value"),
+    Input("controls-and-radio-item", "value"),
+)
+def update_graph(country, radio_value):
+    dff = df[df.country == country]
     return px.line(
         dff,
         x="date",
-        y="new_cases_smoothed_per_million",
+        y=radio_value,
     )
 
 
